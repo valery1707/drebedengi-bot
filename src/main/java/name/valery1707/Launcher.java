35f98a8802292468f34bf2095300289d453bd07d
Valeriy.Vyrva <valery1707@gmail.com>
2016-09-21 10:34:24 +0300

    Update dependency version: org.springframework.boot

--- src/main/java/name/valery1707/Launcher.java
+++ src/main/java/name/valery1707/Launcher.java
@@ -6,7 +6,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
